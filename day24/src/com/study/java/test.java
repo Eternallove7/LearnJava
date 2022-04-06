9add945506d1170dd0e3050b4fbdecb3db3d684b
Eternallove7 <614056287@qq.com>
2022-04-06 16:06:37 +0800

    day24

--- day24/src/com/study/java/test.java
+++
@@ -1,7 +0,0 @@
-package com.study.java;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
--- day24/src/com/study/java/test2.java
+++
@@ -1,9 +0,0 @@
-package com.study.java;
-
-public class test2 {
-}
-
-class Person{
-    private String name;
-    private int age;
-}
