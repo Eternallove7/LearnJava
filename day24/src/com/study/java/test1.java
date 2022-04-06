2cf51dd0362c827ab1c382759ee1091dc22ff6e3
Eternallove7 <614056287@qq.com>
2022-04-06 16:06:03 +0800

    day24 change

---
+++ day24/src/com/study/java/test1.java
@@ -0,0 +1,10 @@
+package com.study.java;
+
+public class test1 {
+}
+
+class Student extends Person{
+    private String name;
+    private int age;
+
+}
---
+++ day24/src/com/study/java/test2.java
@@ -0,0 +1,9 @@
+package com.study.java;
+
+public class test2 {
+}
+
+class Person{
+    private String name;
+    private int age;
+}
